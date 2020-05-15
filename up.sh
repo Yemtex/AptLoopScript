7f79ff317a22029c552df329675f749f9e0a5117
Yemtex <55879864+Yemtex@users.noreply.github.com>
2020-05-15 09:26:03 +0200

    added reboot prompt

--- up.sh
+++ up.sh
@@ -1,7 +1,23 @@
+#!/bin/bash
+
 for i in 1 2
 do
   echo "************************************************"
   echo "**                  STARTING                  **"
   echo "************************************************"
   sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
+done
+
+echo
+echo "************************************************"
+echo "**                  Reboot                    **"
+echo "************************************************"
+
+while true; do
+    read -p "Do you wish to reboot your pc [Y/N]?" yn
+    case $yn in
+        [Yy]* ) reboot;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
 done
